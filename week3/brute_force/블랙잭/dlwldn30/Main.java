756802b0a618f1cd48b72b1631519343b6598b26
dlwldn30 <dlwldn30@naver.com>
2025-03-31 21:32:09 +0900

    블랙잭 30M

---
+++ week3/brute_force/블랙잭/dlwldn30/Main.java
@@ -0,0 +1,38 @@
+package 블랙잭.dlwldn30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int max = 0;
+
+        int[] cards = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if (sum <= M) {
+                        max = Math.max(max, sum);
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
